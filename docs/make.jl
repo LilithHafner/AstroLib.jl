38023ec56e625851479f7a301f7bc233422055a0
Abhro R <5664668+abhro@users.noreply.github.com>
2024-06-17 12:54:12 -0400

    Increase Documenter size restriction

--- docs/make.jl
+++ docs/make.jl
@@ -4,7 +4,12 @@ include("pages.jl")
 makedocs(
     modules = [AstroLib],
     sitename = "AstroLib",
-    pages=pages
+    format = Documenter.HTML(
+        size_threshold = 400 * 1024, # 400 KiB because API reference page is big
+        size_threshold_warn = 200 * 1024,
+        #assets = [],
+    ),
+    pages = pages,
 )
 
 deploydocs(
